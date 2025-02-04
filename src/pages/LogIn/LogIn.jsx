fbb4a6dff97ea5ff4af36ed1bb447e8612b5e610
Wendel Duarte <wendel.duarte@proton.me>
2025-02-04 17:27:39 -0300

    improved LogIn page

--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -1,36 +1,85 @@
 import "./LogIn.css";
-import logo from "/assets/img/logo.png";
 import avatar from "/assets/img/avatar.jpg";
+import avatarB from "/assets/img/avatar_b.jpg";
+import guest from "/assets/img/guest-star.png";
+import keys from "/assets/icons/keys.svg";
+import { useState } from "react";
 
 const LogIn = () => {
+  const [userSelect, setUserSelect] = useState(1);
+  const [accessKey, setAccessKey] = useState("");
+
+  const decrement = () => {
+    if (userSelect === 0) {
+      setUserSelect(2);
+    } else {
+      setUserSelect(userSelect - 1);
+    }
+  };
+
+  const increment = () => {
+    if (userSelect === 2) {
+      setUserSelect(0);
+    } else {
+      setUserSelect(userSelect + 1);
+    }
+  };
+
+  const handleKeyChange = (e) => {
+    setAccessKey(e.target.value);
+  };
+
+  const handleAccess = (e) => {
+    e.preventDefault();
+
+    if (import.meta.env.VITE_ACCESS_KEY !== accessKey) {
+      alert("incorrect access key");
+    } else {
+      alert("Correct access key");
+    }
+  };
+
   return (
     <section id="LogIn">
-      <form id="login-form" action="">
+      <form id="login-form" action="" onSubmit={(e) => handleAccess(e)}>
         <div id="login-user-wrp">
-          <button type="button">
+          <button type="button" onClick={decrement}>
             <img
               src="/assets/icons/caret-left.svg"
               alt="Seta apontada para a esquerda"
             />
           </button>
           <div id="login-user-display">
-            <div>
-              <img src={avatar} alt="Imagem Wendel" />
+            <div className={userSelect === 0 ? "selected" : ""}>
+              <img src={avatar} alt="Imagem de avatar Wendel" />
               <p>Wendel</p>
             </div>
+            <div className={userSelect === 1 ? "selected" : ""}>
+              <img src={guest} alt="Imagem de avatar convidado" />
+              <p>Convidado</p>
+            </div>
+            <div className={userSelect === 2 ? "selected" : ""}>
+              <img src={avatarB} alt="Imagem de avatar Bianca" />
+              <p>Bianca</p>
+            </div>
           </div>
-          <button type="button">
+          <button type="button" onClick={increment}>
             <img
               src="/assets/icons/caret-left.svg"
               alt="Seta apontada para a direita"
             />
           </button>
         </div>
-        <input
-          id="login-password"
-          type="password"
-          placeholder="chave de acesso"
-        />
+        <div id="login-password-wrp">
+          <img src={keys} alt="Icone de chaves" />
+          <input
+            id="login-password"
+            type="password"
+            placeholder="chave de acesso"
+            value={accessKey}
+            onChange={handleKeyChange}
+          />
+        </div>
         <input id="login-access" type="submit" value="Acessar" />
       </form>
     </section>
