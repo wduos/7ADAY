7423e29e6671a5cd52e96ed92b86217d19996a90
Wendel Duarte <wendel.duarte@proton.me>
2025-02-04 02:04:10 -0300

    created initial LogIn design

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Router from "./routes/Router.jsx";
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("app")).render(
   <StrictMode>
     <Router />
   </StrictMode>
--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -1,12 +1,39 @@
 import "./LogIn.css";
 import logo from "/assets/img/logo.png";
+import avatar from "/assets/img/avatar.jpg";
 
 const LogIn = () => {
   return (
-    <>
-      <h1>Hello World</h1>
-      <img id="logo" src={logo} alt="7ADAY logo" />
-    </>
+    <section id="LogIn">
+      <form id="login-form" action="">
+        <div id="login-user-wrp">
+          <button type="button">
+            <img
+              src="/assets/icons/caret-left.svg"
+              alt="Seta apontada para a esquerda"
+            />
+          </button>
+          <div id="login-user-display">
+            <div>
+              <img src={avatar} alt="Imagem Wendel" />
+              <p>Wendel</p>
+            </div>
+          </div>
+          <button type="button">
+            <img
+              src="/assets/icons/caret-left.svg"
+              alt="Seta apontada para a direita"
+            />
+          </button>
+        </div>
+        <input
+          id="login-password"
+          type="password"
+          placeholder="chave de acesso"
+        />
+        <input id="login-access" type="submit" value="Acessar" />
+      </form>
+    </section>
   );
 };
 
